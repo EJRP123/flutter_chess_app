3026e4d1895bc6e9fc2aae0b7a769fde15df2692
EJRP123 <e1j2r3p4@gmail.com>
2023-03-17 16:05:50 -0400

    Removed the C engine source code from the repo

--- engine_src/testing/chessGameEmulator.h
+++
@@ -1,8 +0,0 @@
-#ifndef CHESS_GAME_EMLATOR_H
-#define CHESS_GAME_EMLATOR_H
-
-#include "../src/moveGenerator.h"
-
-void makeMove(unsigned short move, GameState* state);
-
-#endif
--- engine_src/testing/logChessStructs.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOGCHESSSTRCUTS_H
-#define LOGCHESSSTRCUTS_H
-
-void printMove(unsigned short move);
-
-void printMoveToAlgebraic(unsigned short move);
-
-void printBoard(int board[64]);
-#endif
